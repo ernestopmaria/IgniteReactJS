2e2ff52f0ca7daf6c5a2a887f2c87a78acb6be97
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-08-04 16:27:18 +0100

    add sass file

---
+++ 01-GITHUB-EXPLORER/src/components/RepositoryList.jsx
@@ -0,0 +1,29 @@
+
+
+export function RepositoryList(){
+    return(
+        <section className="repository-list">
+            <h1>Lista de repositorios</h1>
+            <ul>
+                <li>
+                    <strong>Unform          
+                    </strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar repositorios</a>
+                </li>
+                <li>
+                    <strong>Unform          
+                    </strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar repositorios</a>
+                </li>
+                <li>
+                    <strong>Unform          
+                    </strong>
+                    <p>Forms in React</p>
+                    <a href="">Acessar repositorios</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
