e2f8941d6df2b67562163577672faf64126e23da
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-08-05 15:56:16 +0100

    using Useffect

--- 01-GITHUB-EXPLORER/src/components/RepositoryList.jsx
+++ 01-GITHUB-EXPLORER/src/components/RepositoryList.jsx
@@ -1,28 +1,30 @@
+import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss'
+import { useEffect, useState } from "react";
+
+const repository={
+    name:"unform",
+    description:"Forms in React",
+    link:"https://github.com/unform/unform"
+}
 
 
 export function RepositoryList(){
+
+    const[repositories, setRepositories] = useState([])
+
+    useEffect(()=>{
+        fetch("https://api.github.com/orgs/rocketseat/repos")
+        .then(response=>response.json())
+        .then(data=>setRepositories(data))
+    },[])
     return(
         <section className="repository-list">
             <h1>Lista de repositorios</h1>
-            <ul>
-                <li>
-                    <strong>Unform          
-                    </strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar repositorios</a>
-                </li>
-                <li>
-                    <strong>Unform          
-                    </strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar repositorios</a>
-                </li>
-                <li>
-                    <strong>Unform          
-                    </strong>
-                    <p>Forms in React</p>
-                    <a href="">Acessar repositorios</a>
-                </li>
+            <ul>             
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
+                <RepositoryItem repository={repository}/>
             </ul>
         </section>
     )
--- 01-GITHUB-EXPLORER/src/index.jsx
+++ 01-GITHUB-EXPLORER/src/index.jsx
@@ -1,6 +1,6 @@
 import  { render } from 'react-dom'
 import {App} from './App'
-import { RepositoryList } from './components/RepositoryList'
+
 
 
 render(
