59d47f50e56c619183634254f3b14bf67e0c9b22
Ernesto Maria <58423237+ernestopmaria@users.noreply.github.com>
2021-10-11 19:02:35 +0100

    add new footer

---
+++ ignews/src/components/Footer copy/index.tsx
@@ -0,0 +1,11 @@
+
+import styles from './styles.module.scss'
+
+export function Footer(){
+    return(
+        <div className={styles.headerContainer}>
+          <span className={styles.headerContent}> © 2021. All Rights Reserved.</span>
+            </div>
+      
+    )
+}
